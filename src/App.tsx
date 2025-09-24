f5d8fee423e1997a6789066fe2544b6bd153accc
Sameer-Saleem1 <sameersaleemq75@gmail.com>
2025-09-24 20:55:57 +0500

    initializing the mashvisor-frontend-task

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-semibold text-center">
+        Real Estate Listing Dashboard
+      </h1>
+    </>
+  );
+}
+
+export default App;
